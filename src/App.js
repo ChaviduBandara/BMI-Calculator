3461127bc23091b8e41a3a6ff72e97495d3fd641
ChaviduBandara <152202463+ChaviduBandara@users.noreply.github.com>
2024-10-29 22:44:49 +0530

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,29 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <div className='container'>
+        <h2>BMI Calculator</h2>
+        <form>
+          <div>
+            <label>Weight (kg)</label>
+            <input type='text' placeholder='Enter the weight' value={weight}/>
+          </div>
+          <div>
+            <label>Height (cm)</label>
+            <input type='text' placeholder='Enter the height' value={height}/>
+          </div>
+
+          <div>
+            <button className='btn' type='submit'>Submit</button>
+            <button className='btn btn-outline' onClick={reload} type='submit'>Reload</button>
+          </div>
+
+          <div className='center'>
+            <h3>Your BMI is : {bmi}</h3>
+            <p>{message}</p>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
